9bac8eb219447573671b703cca7f7fe3a3c8f1c3
Aldo Quintero <aldoq95@gmail.com>
2022-03-04 16:36:02 -0800

    alert system set up

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,36 @@ import Navbar from './components/layouts/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
-
+import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layouts/Alerts';
 import './App.css';
 // react router dom new jsx format
+// AuthState protects the routes
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/about' element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Routes>
+                  <Route exact path='/' element={<Home />} />
+                  <Route exact path='/about' element={<About />} />
+                  <Route exact path='/register' element={<Register />} />
+                  <Route exact path='/login' element={<Login />} />
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
